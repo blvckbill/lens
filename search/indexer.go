bb6585e7e81cf1e425351b0e4a98fc9641e79eda
Abdulroqeeb Bakre <walebakre10@gmail.com>
2026-06-18 23:47:26 +0100

    feat(invertedIndex): add sort-based inverted index and secure doc registry
    Implements the core indexing pipeline for Lens. Features bidirectional docID
    mapping, primary/secondary sorting, and memory-efficient linear merging with
    O(1) duplicate checks.

--- search/indexer.go
+++ search/indexer.go
@@ -2,16 +2,74 @@ package search
 
 import (
 	"bufio"
+	"cmp"
 	"log"
 	"os"
+	"slices"
 	"strings"
 	"unicode"
 )
 
-func Indexer(documents ...string) map[string]map[string]int {
-	var indexer = make(map[string]map[string]int, 10)
+type Postings struct {
+	docFreq int
+	docIds  []int
+}
+
+type Registry struct {
+	docs     []string
+	docToID  map[string]int
+	idTodoc  map[int]string
+	postings map[string]Postings
+	nextID   int
+}
+
+type WordIndex struct {
+	word  string
+	docId int
+}
+
+func NewRegistry() *Registry {
+	return &Registry{
+		docs:     make([]string, 0),
+		docToID:  make(map[string]int),
+		idTodoc:  make(map[int]string),
+		postings: make(map[string]Postings),
+		nextID:   1,
+	}
+}
+
+func (r *Registry) InvertedIndex(path string) map[string]Postings {
+	files, err := os.ReadDir(path)
+	if err != nil {
+		log.Printf("Error occured while trying to locate %s, path not found", path)
+	}
+	for i := range len(files) {
+		r.docs = append(r.docs, files[i].Name())
+	}
+
+	wordIndex := r.PairWordsAndDocId(path, r.docs...)
+	sortedPairs := r.SortPairsByWord(wordIndex)
+	postingsList := r.TermDictionary(sortedPairs)
+
+	return postingsList
+}
+
+func (r *Registry) AddDocument(filename string) int {
+	_, ok := r.docToID[filename]
+	if !ok {
+		r.docToID[filename] = r.nextID
+		r.idTodoc[r.nextID] = filename
+		r.nextID++
+	}
+	return r.docToID[filename]
+}
+
+func (r *Registry) PairWordsAndDocId(path string, documents ...string) []WordIndex {
+	var pairs []WordIndex
 	for _, document := range documents {
-		file := openDocument(document)
+		file := openDocument(path, document)
+		// map doc to id and return the id
+		docId := r.AddDocument(file.Name())
 
 		scanner := bufio.NewScanner(file)
 		scanner.Split(bufio.ScanWords)
@@ -28,21 +86,64 @@ func Indexer(documents ...string) map[string]map[string]int {
 			}
 			word := strings.ToLower(b.String())
 
-			if indexer[word] == nil {
-				indexer[word] = make(map[string]int)
+			if word == "" {
+				continue
 			}
-			indexer[word][document] += 1
 
+			// map word to doc id
+			w := WordIndex{
+				word:  word,
+				docId: docId,
+			}
+
+			// append the pair (word, docid) to slice pairs
+			pairs = append(pairs, w)
 		}
 		file.Close()
 	}
-	return indexer
+	// return list of all pairs
+	return pairs
 }
 
-func openDocument(document string) *os.File {
-	file, err := os.Open(document)
+func (r *Registry) SortPairsByWord(pairs []WordIndex) []WordIndex {
+	slices.SortFunc(pairs, func(a, b WordIndex) int {
+		if n := strings.Compare(a.word, b.word); n != 0 {
+			return n
+		}
+		return cmp.Compare(a.docId, b.docId)
+	})
+	return pairs
+}
+
+func (r *Registry) TermDictionary(sorted_pairs []WordIndex) map[string]Postings {
+	for i := range len(sorted_pairs) {
+		word := sorted_pairs[i].word
+		docId := sorted_pairs[i].docId
+
+		v, ok := r.postings[word]
+
+		if !ok {
+			r.postings[word] = Postings{
+				docFreq: 1,
+				docIds:  []int{docId},
+			}
+		} else {
+			if v.docIds[len(v.docIds)-1] == docId {
+				continue
+			}
+			r.postings[word] = Postings{
+				docFreq: v.docFreq + 1,
+				docIds:  append(v.docIds, docId),
+			}
+		}
+	}
+	return r.postings
+}
+
+func openDocument(path, document string) *os.File {
+	file, err := os.OpenInRoot(path, document)
 	if err != nil {
-		log.Fatalf("Error reading from document %s", document)
+		log.Fatalf("Error reading from %s", document)
 	}
 
 	return file
